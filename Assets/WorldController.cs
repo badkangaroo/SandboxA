c2624c3ac832a732e113559afede222ee686caf3
alex okita <alex@occipital.com>
2015-01-14 21:52:56 -0800

    added some sphere spawning action to a hive.

---
+++ Assets/WorldController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour
+{
+	public GameObject BugHive;
+    public float timeInterval = 5;
+    float nextTime;
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject go = GameObject.Instantiate(BugHive) as GameObject;
+        nextTime = Time.fixedTime + timeInterval;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Time.fixedTime > nextTime)
+        {
+            Debug.Log("bug");
+            GameObject bug = GameObject.CreatePrimitive(PrimitiveType.Sphere) as GameObject;
+            bug.AddComponent<Rigidbody>();
+            Vector3 randomDirection = new Vector3(Random.Range(-1,1),0,Random.Range(-1,1));
+            bug.rigidbody.AddForce(randomDirection);
+            nextTime = Time.fixedTime + timeInterval;
+        }
+	}
+}
